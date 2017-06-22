683f7ee2e624eead0bde21d6a91bf3329160629e
Brett Heenan <brettrheenan@gmail.com>
2017-06-22 16:24:36 +0200

    remove test folder

--- test/system/sub_reddit_comments_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class SubRedditCommentsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit sub_reddit_comments_url
-  #
-  #   assert_selector "h1", text: "SubRedditComment"
-  # end
-end
